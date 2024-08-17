e892977b0e9551d341adc208b4f69e3d31e6e5f5
Aailmz <mirzazubaridjunaid@gmail.com>
2024-08-17 18:58:56 +0700

    Fix Landing Page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\TransactionController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Authorization.login');
 });
 
 //Auth
